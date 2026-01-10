fb43d029e637f11aa99ed6f73ddc10f0b78d5732
Muhammad Nauman <nauman33183@gmail.com>
2026-01-10 11:25:52 +0500

    env hardcoded remove

--- packages/config/index.ts
+++ packages/config/index.ts
@@ -1,8 +1,8 @@
 export const PORTS = {
-    GATEWAY: Number(process.env.GATEWAY_PORT) || 3000,
-    AUTH: Number(process.env.AUTH_PORT) || 3001,
-    CATEGORY: Number(process.env.CATEGORY_PORT) || 3002,
-    ORDER: Number(process.env.ORDER_PORT) || 5000
+    GATEWAY: Number(process.env.GATEWAY_PORT),
+    AUTH: Number(process.env.AUTH_PORT),
+    CATEGORY: Number(process.env.CATEGORY_PORT),
+    ORDER: Number(process.env.ORDER_PORT)
 };
 
 export const API_URL = `http://localhost:${PORTS.GATEWAY}`;
